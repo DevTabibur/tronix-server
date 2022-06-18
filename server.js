a04d42acf5a29b623927a78a60411c8a4a1c1f58
dexterousprogrammers <tobiburrohman2@gmail.com>
2022-06-18 11:04:22 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// user:tronix
+// pass:JBEWCPcjTsUJuzkf
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.hc4xz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("MOngo Connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get('/', (req, res)=>{
     res.send("Hello World!")
