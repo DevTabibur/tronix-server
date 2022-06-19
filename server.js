987ce1968c00358888ec1ffeeff2928f76e511c6
dexterousprogrammers <tobiburrohman2@gmail.com>
2022-06-19 13:46:33 +0600

    loading blogs and products

--- server.js
+++ server.js
@@ -17,12 +17,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+  try{
+    await client.connect();
+    const BlogCollections = client.db("tronix").collection("blogs");
+    const ProductCollections = client.db("tronix").collection("products");
 
+
+    // load all blogs
+    app.get('/blogs', async (req, res)=>{
+      // console.log(req.body);
+      const query={};
+      const cursor = BlogCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // load all products
+    app.get('/products', async (req, res)=>{
+      // console.log(req.body);
+      const query={};
+      const cursor = ProductCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+  }
+  finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res)=>{
-    res.send("Hello World!")
+  res.send("Hello World!")
 });
-
 
 // port
 app.listen(port, ()=>{
